05bf25b5773750ccf91c82401c2df73a935c2421
Sid Agrawal <sid@sid-agrawal.ca>
2023-08-19 22:14:53 -0700

    remove qemu target

--- hello-c4/run.sh
+++ hello-c4/run.sh
@@ -1,1 +1,1 @@
-make BUILD_DIR=.  SEL4CP_SDK=../../../release/sel4cp-sdk-1.2.6  SEL4CP_CONFIG=debug  SEL4CP_BOARD=qemu_arm_virt run
+make BUILD_DIR=.  SEL4CP_SDK=../../../release/sel4cp-sdk-1.2.6  SEL4CP_CONFIG=debug  SEL4CP_BOARD=odroidc4
