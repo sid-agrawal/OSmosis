94d9e1da4baf4ce3452687b967d0662076b01f86
Sid Agrawal <sid@sid-agrawal.ca>
2023-10-17 09:55:52 -0700

    fix app name in BLOB

--- OSmosisRootServer/app/app.c
+++ OSmosisRootServer/app/app.c
@@ -1,9 +1,10 @@
 #include<sel4/sel4.h>
+#include<utils/mk-printf.h>
 
 seL4_IPCBuffer __sel4_ipc_buffer_obj;
 __thread seL4_IPCBuffer *__sel4_ipc_buffer = &__sel4_ipc_buffer_obj;
 int
 main () {
-        int x  = 2;
-        return x;
+       while(1);
+       printf("Hello World\n");
 }
--- OSmosisRootServer/hello.c
+++ OSmosisRootServer/hello.c
@@ -27,6 +27,11 @@ extern char _test_blob_end[];
 extern char _cpio_archive[];
 extern char _cpio_archive_end[];
 
+#define ALLOCATOR_VIRTUAL_POOL_SIZE ((1 << seL4_PageBits) * 4000)
+
+/* allocator static pool */
+#define ALLOCATOR_STATIC_POOL_SIZE ((1 << seL4_PageBits) * 20)
+
 static char
 hexchar(unsigned int v)
 {
@@ -125,9 +130,10 @@ void test_malloc() {
      }
 }
 
-#define VIRTUAL_START 0xA0000000
-#define MEM_POOL_SIZE (1*1024*1024)
-static char initial_mem_pool[MEM_POOL_SIZE];
+#define VIRTUAL_START (void *)0xA0000000
+#define ALLOCATOR_VIRTUAL_POOL_SIZE ((1 << seL4_PageBits) * 4000)
+#define ALLOCATOR_STATIC_POOL_SIZE ((1 << seL4_PageBits) * 20)
+static char initial_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
 
 /* stack for the new thread */
 #define THREAD_2_STACK_SIZE 4096
@@ -145,22 +151,100 @@ void thread_2(void) {
     while (1);
 }
 
-vspace_t vspace;
-UNUSED static sel4utils_alloc_data_t data;
+static vspace_t vspace;
+static vka_t vka;
+static sel4utils_alloc_data_t data;
 
-#define ALLOCATOR_VIRTUAL_POOL_SIZE (BIT(seL4_PageBits) * 100)
+static void init_allocator(vka_t *vka, vspace_t *vspace, sel4utils_alloc_data_t *data,
+                           seL4_CPtr root_cnode, seL4_CPtr vspace_cap,
+                           size_t cnode_size_bits, seL4_CPtr empty_slot_start, seL4_CPtr empty_slot_end,
+                           size_t initial_mem_pool_size, void *initial_mem_pool)
+
+{
+     UNUSED int error;
+     UNUSED reservation_t virtual_reservation;
+
+     /* initialise allocator */
+     allocman_t *allocator = bootstrap_use_current_1level(root_cnode,
+                                                          cnode_size_bits,
+                                                          empty_slot_start,
+                                                          empty_slot_end,
+                                                          initial_mem_pool_size,
+                                                          initial_mem_pool);
+     if (allocator == NULL)
+     {
+          ZF_LOGF("Failed to bootstrap allocator");
+     }
+     printf("---Bootstraped allocator\n");
+     allocman_make_vka(vka, allocator);
+
+     /* fill the allocator with untypeds */
+     seL4_CPtr slot = 0x20;
+     // unsigned int size_bits_index;
+     // size_t size_bits;
+     cspacepath_t path;
+     //     for (slot = init_data->untypeds.start, size_bits_index = 0;
+     //          slot <= init_data->untypeds.end;
+     //          slot++, size_bits_index++) {
+
+     vka_cspace_make_path(vka, slot, &path);
+     /* allocman doesn't require the paddr unless we need to ask for phys addresses,
+      * which we don't. */
+     size_t size_bits = 29;
+     path.capDepth = 10;
+     error = allocman_utspace_add_uts(allocator, 1, &path, &size_bits, NULL,
+                                      ALLOCMAN_UT_KERNEL);
+     if (error)
+     {
+          ZF_LOGF("Failed to add untyped objects to allocator");
+     }
+     printf("---Add UTS\n");
+     //     }
+
+     /* add any arch specific objects to the allocator */
+     //     arch_init_allocator(env, init_data);
+
+     /* create a vspace */
+
+     error = sel4utils_bootstrap_vspace(
+         vspace,
+         data,
+         vspace_cap,
+         vka,
+         NULL, NULL, 0);
+     if (error)
+     {
+          ZF_LOGF("Failed to bootstrap vspace");
+     }
+     printf("---VSpace Bootstrapped\n");
+
+     /* switch the allocator to a virtual memory pool */
+     virtual_reservation = vspace_reserve_range_at(vspace, VIRTUAL_START, ALLOCATOR_VIRTUAL_POOL_SIZE,
+                                                   seL4_AllRights, 1);
+     if (virtual_reservation.res == 0)
+     {
+          ZF_LOGF("Failed to switch allocator to virtual memory pool");
+     }
+
+     printf("---VSpace Range reserved\n");
+     bootstrap_configure_virtual_pool(allocator, VIRTUAL_START, ALLOCATOR_VIRTUAL_POOL_SIZE, vspace_cap);
+     printf("---Configured Virtual Pool\n");
+}
 void test_process_create() {
 
      /*
           Create an allocator
      */
 
-     int error;
-    allocman_t *allocman;
+
     seL4_CPtr root_cnode = 0x7;
     size_t cnode_size_bits = 10;
     seL4_CPtr empty_slot_start = 0x40;
     seL4_CPtr empty_slot_end = 0x400;
+    seL4_CPtr vspace_cap = 0x3;
+#if 0
+     int error;
+     allocman_t *allocman;
     allocman = bootstrap_use_current_1level(root_cnode,
                                              cnode_size_bits,
                                              empty_slot_start,
@@ -174,7 +258,7 @@ void test_process_create() {
         cspacepath_t slot = allocman_cspace_make_path(allocman, 0x20);
         /*check for error*/
         cspacepath_t_print(&slot);
-#if 1
+        slot.capDepth = 10;
         size_t size_bits = 29;
         uintptr_t paddr = 0x80000000;
         error = allocman_utspace_add_uts(allocman, 1, &slot, &size_bits, &paddr, ALLOCMAN_UT_KERNEL);
@@ -183,40 +267,44 @@ void test_process_create() {
    // }
 //     error = allocman_fill_reserves(allocman);
 //     assert(!error);
-#endif
 
     /*
           Get a vka interface for the allcotor
     */
-     vka_t vka;
     allocman_make_vka(&vka, allocman);
 
    /*
           Get curent PDs CSpace and VSpace
    */
     //seL4_CPtr root_cnode = 0x7;
-    seL4_CPtr vspace_cap = 0x3;
 
     error = sel4utils_bootstrap_vspace_leaky(&vspace, &data, vspace_cap, &vka, NULL);
      assert(error == 0);
 
 
     /* fill the allocator with virtual memory */
-    void *vaddr;
     UNUSED reservation_t virtual_reservation;
-    virtual_reservation = vspace_reserve_range(&vspace,
-                                               ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1, &vaddr);
-    if (virtual_reservation.res == NULL)
-    {
-         printf("Failed to reserve a chunk of memory.\n");
-         assert(0);
-    }
-    printf("Reserved a chunk chunk of memory starting at: 0x%p \n", vaddr);
-    bootstrap_configure_virtual_pool(allocman, vaddr,
+    virtual_reservation = vspace_reserve_range_at(&vspace, (void *)VIRTUAL_START,
+                                                   ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1);
+    printf("Reserved a chunk chunk of memory starting at: 0x%p \n", VIRTUAL_START);
+    bootstrap_configure_virtual_pool(allocman, (void *) VIRTUAL_START,
                                       ALLOCATOR_VIRTUAL_POOL_SIZE, vspace_cap);
 
+//     error = allocman_fill_reserves(allocman);
+//     assert(!error);
 
     printf("Bootstraped virtual pool\n");
+#endif
+     printf("------Start: INIT ALLOCATOR----\n");
+     init_allocator(&vka, &vspace, &data,
+     root_cnode,
+     vspace_cap,
+     cnode_size_bits,
+     empty_slot_start,
+     empty_slot_end,
+     sizeof(initial_mem_pool),
+     initial_mem_pool);
+     printf("------END: INIT ALLOCATOR----\n");
      /*
           Setup the config explictly
      */
@@ -224,8 +312,8 @@ void test_process_create() {
     /*
          Get this for the confi
     */
-#if 0
     seL4_CPtr current_tcb = 0x06;
+    seL4_CPtr current_sch_control=  0x21;
     seL4_CPtr current_asid_pool = 0x22;
 
     UNUSED sel4utils_process_config_t config = {
@@ -245,11 +333,22 @@ void test_process_create() {
         .num_reservations = 0,
 
         .create_fault_endpoint = true,
-        .sched_params.auth = current_tcb,
+        .sched_params = {
+          .create_sc = true,
+          .auth = current_tcb,
+          .sched_ctrl =  current_sch_control,
+          .budget = 1000000,
+          .period = 1000000,
+          .priority = 254,
+        },
+
+
         .asid_pool = current_asid_pool,
     };
 
+#if 1
 
+     int error;
      //    vka_object_t root_pd = {0};
      //    error = vka_alloc_vspace_root(&vka, &root_pd);
      //    if (error) {
@@ -273,56 +372,56 @@ void test_process_create() {
     {
          printf("Success configuring new process.\n");
     }
-    NAME_THREAD(new_process.thread.tcb.cptr, "dynamic-3: process_2");
+     NAME_THREAD(new_process.thread.tcb.cptr, "app");
+
+         printf("Success naming new process.\n");
+
+         /* create an endpoint */
+         vka_object_t ep_object = {0};
+         error = vka_alloc_endpoint(&vka, &ep_object);
+         if (error)
+         {
+              ZF_LOGF("Failed to allocate new endpoint object.\n");
+              assert(0);
+         }
+         cspacepath_t ep_cap_path;
+         seL4_CPtr new_ep_cap = 0;
+         vka_cspace_make_path(&vka, ep_object.cptr, &ep_cap_path);
+         new_ep_cap = sel4utils_mint_cap_to_process(&new_process, ep_cap_path,
+                                                    seL4_AllRights, 0x55); /*badge*/
+          if (new_ep_cap == 0) {
+
+         ZF_LOGF("Failed to mint a badged copy of the IPC endpoint into the new thread's CSpace.\n"
+                                     "\tsel4utils_mint_cap_to_process takes a cspacepath_t: double check what you passed.\n");
+
+          }
+
+
+         printf("NEW CAP SLOT: %" PRIxPTR ".\n", ep_cap_path.capPtr);
+
+         seL4_Word argc = 1;
+         char string_args[argc][WORD_STRING_SIZE];
+         char *argv[argc];
+         sel4utils_create_word_args(string_args, argv, argc, 42);
+
+         error = sel4utils_spawn_process_v(&new_process, &vka, &vspace, argc, (char **)&argv, 1);
+         if (error)
+         {
+              ZF_LOGF("Failed to spawn and start the new thread.\n"
+                      "\tVerify: the new thread is being executed in the root thread's VSpace.\n"
+                      "\tIn this case, the CSpaces are different, but the VSpaces are the same.\n"
+                      "\tDouble check your vspace_t argument.\n");
+
+    }
+
+    /* we are done, say hello */
+    printf("main: hello world\n");
+    seL4_DebugDumpScheduler();
+    while(1);
 #endif
-
-
-
-     /*
-          Create a cap
-     */
-
-     vka_object_t some_edp = {0};
-     error = vka_alloc_endpoint(&vka, &some_edp);
-     if (error)
-     {
-          printf("Failed to allocate new endpoint.\n");
-          assert(0);
-     }
-     else
-     {
-          printf("Success allocating endpoint at slot: %lx, type: %d\n",
-                 some_edp.cptr,
-                 seL4_DebugCapIdentify(some_edp.cptr));
-     }
-   cspacepath_t src_path = {0};
-   vka_cspace_make_path(&vka, some_edp.cptr, &src_path);
-   cspacepath_t_print(&src_path);
-
-    /*
-          Mint it
-    */
-   cspacepath_t dst_minted_edp = {0};
-   error = vka_cspace_alloc_path(&vka, &dst_minted_edp);
-   if (error)
-   {
-        printf("Failed to allocate new dst slot.\n");
-        assert(0);
-   }
-   else
-   {
-        printf("Success allocating dst path: %lx\n", dst_minted_edp.capPtr);
-        cspacepath_t_print(&dst_minted_edp);
-   }
-     error = vka_cnode_mint(&dst_minted_edp, &src_path, seL4_AllRights, 0);
-     if (error ) {
-          printf("Failed to mint endpoint.\n");
-          assert(0);
-     } else {
-          printf("Success minting endpoint.\n");
-     }
-
 }
+
+
 void test_thread_create() {
 
      /*
@@ -467,6 +566,7 @@ void test_thread_create() {
               Setup new priority, entry, stack-pointer etc.
         */
         seL4_CPtr current_tcb = 0x06;
+        NAME_THREAD(current_tcb, "OSmosisRootServer");
         error = seL4_TCB_SetPriority(tcb_object.cptr, current_tcb, 254);
         if (error)
         {
@@ -600,6 +700,10 @@ init(void)
 
      }
 
+     /*
+     0x20 untyped
+     0x21 sched control
+     0x22 asid pool*/
 
      seL4_Word seL4_Fault_SendUntyped = 21;
      // Send a message to the monitor asking for an untyped.
@@ -662,7 +766,7 @@ init(void)
 
 #endif
 
-     for (int i = 0x20; i < 0x25 ;i++){
+     for (int i = 0x20; i < 0x30 ;i++){
           puthex32(i);
           sel4cp_dbg_puts("> ");
           puthex32(seL4_DebugCapIdentify(i));
@@ -676,10 +780,30 @@ init(void)
 
 
 
+//     error =  seL4_CNode_Mint(
+//                /* _service */    7,//  dest->root,
+//                /* dest_index */  0x25,//  dest->capPtr,
+//                /* destDepth */   10,// dest->capDepth,
+//                /* src_root */    7,//  src->root,
+//                /* src_index */   0x21,//  src->capPtr,
+//                /* src_depth */   10,//  src->capDepth,
+//                /* rights */      seL4_AllRights,
+//                /* badge  */        0
+//            );
+//      for (int i = 0x20; i < 0x30 ;i++){
+//           puthex32(i);
+//           sel4cp_dbg_puts("> ");
+//           puthex32(seL4_DebugCapIdentify(i));
+//           if (i == 0x20) {
+//                 sel4cp_dbg_puts("<--");
+//           }
+//           sel4cp_dbg_puts("\n");
+
+//      }
 
      //test_symbols();
      //test_malloc10();
-     populateBootInfo();
+     // populateBootInfo();
      //test_thread_create();
      test_process_create();
 }
