5281e779e72d98a1583e63e660a06df2ae5892b9
Sid Agrawal <sid@sid-agrawal.ca>
2024-02-19 22:12:53 -0800

    add supercommit file

---
+++ supercommit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+if [ -z "$1" ]; then
+    echo "You need to provide a commit message"
+    exit
+fi
+
+git submodule foreach "
+    git add -A .
+    git update-index --refresh
+    commits=\$(git diff-index HEAD)
+    if [ ! -z \"\$commits\" ]; then
+        git commit -am \"$1\"
+    fi"
+
+git add -A .
+git commit -am "$1"
