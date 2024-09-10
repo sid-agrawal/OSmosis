a1d91cae5216affc75e170854265690b3744c261
Sid Agrawal <sid@sid-agrawal.ca>
2024-09-10 12:49:58 -0700

    Remove gdrive URL in import script

--- scripts/model_state/import_csv.py
+++ scripts/model_state/import_csv.py
@@ -5,10 +5,10 @@
 from neo4j import GraphDatabase
 import argparse
 import configparser
+import os
 
 parser = argparse.ArgumentParser("import_csv")
 parser.add_argument("-f", "--file", help="a LOCAL CSV file to upload")
-parser.add_argument("-i", "--uidx", help="index of a URL in public_urls to upload", type=int)
 parser.add_argument("-c", "--color", help="modifies node types for colored output in neo4j (does not work for metrics)", 
                     action='store_true', default=False)
 args = parser.parse_args()
@@ -18,17 +18,6 @@ config.read("config.txt")
 
 URI = config.get("neo4j", "url")
 AUTH = (config.get("neo4j", "user"), config.get("neo4j", "pass"))
-
-# Public CSV file for upload
-public_urls = [
-"https://drive.google.com/uc?id=1jfZn5dlESzoPav2b2Gxm7BYHNTgJ_Fpm&export=download", #0 proc basic, lindt
-"https://drive.google.com/uc?id=1SN8vHF8hDlkH-4QVLxFMUTcnnSW91qLq&export=download", #1 proc malloc, lindt
-"https://drive.google.com/uc?id=1-TfOq-eGQQeFnA1hNVSeze-SD7dBG6kh&export=download", #2 proc mmap, lindt
-"",
-"",
-"https://drive.google.com/uc?id=1h-F2jcFYY2680G_jMvOLyUw_MhsqdYtF&export=download", #5 proc mmap, ubuntu 
-"https://drive.google.com/uc?id=10UlRTsdnxyLU64gNpMWzdYhvmnUC--wR&export=download", #6, 
-]
 
 def upload_csv(file_url):
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
@@ -50,7 +39,8 @@ def upload_csv(file_url):
                 LOAD CSV WITH HEADERS FROM '%s' AS row
                 WITH row
                 WHERE row.NODE_TYPE = "PD"
-                CALL apoc.merge.node([row.NODE_TYPE], {NODE_TYPE: row.NODE_TYPE, ID: row.NODE_ID, DATA: row.DATA, EXTRA: coalesce(row.EXTRA, "0")}, {}, {})
+                CALL apoc.merge.node([row.NODE_TYPE], {NODE_TYPE: row.NODE_TYPE, 
+                   ID: row.NODE_ID, DATA: row.DATA, EXTRA: coalesce(row.EXTRA, "0")}, {}, {})
                 YIELD node
                 RETURN null;
                 """ % (file_url)
@@ -61,7 +51,8 @@ def upload_csv(file_url):
                 LOAD CSV WITH HEADERS FROM '%s' AS row
                 WITH row
                 WHERE row.NODE_TYPE = "RESOURCE" OR row.NODE_TYPE = "RESOURCE_SPACE"
-                CALL apoc.merge.node([%s], {NODE_TYPE: row.NODE_TYPE, ID: row.NODE_ID, DATA: row.DATA, EXTRA: coalesce(row.EXTRA, "0")}, {}, {})
+                CALL apoc.merge.node([%s], {NODE_TYPE: row.NODE_TYPE, ID: row.NODE_ID, 
+                   DATA: row.DATA, EXTRA: coalesce(row.EXTRA, "0")}, {}, {})
                 YIELD node
                 RETURN null;
                 """ % (file_url, 'row.NODE_TYPE + "_" + COALESCE(row.DATA, "")' if args.color else 'row.NODE_TYPE')
@@ -87,14 +78,15 @@ def upload_csv(file_url):
         print("Complete")
 
 if __name__ == "__main__":
-    if args.file is not None and len(args.file) > 0:
+    if args.file is not None and len(args.file) > 0  and os.path.isfile(args.file) :
         file_url = "file:///" + args.file
-    elif args.uidx is not None:
-        file_url = public_urls[args.uidx]
+        print(
+                "Uploading file", args.file
+                )
     else:
         print(
-            "Please provide either a local CSV file or an index for a remote CSV URL in the public_urls array"
-        )
+                "Please provide either a local CSV file"
+                )
         parser.parse_args(['-h'])
         raise SystemExit()
     upload_csv(file_url)
