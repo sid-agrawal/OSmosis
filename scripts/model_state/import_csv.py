68beadbf423fcb2df8473003c993048839dd1fca
Sid Agrawal <sid@sid-agrawal.ca>
2024-10-28 15:54:10 -0700

    update import script

--- scripts/model_state/import_csv.py
+++ scripts/model_state/import_csv.py
@@ -1,17 +1,12 @@
 # Import a CSV file to Neo4j
 # Usage: pass the command line argument for the public url index to upload
-# eg: python import_csv.py 3
+# eg: python import_csv.py --file local-file --copy --append
 
 from neo4j import GraphDatabase
 import argparse
 import configparser
 import os
-
-parser = argparse.ArgumentParser("import_csv")
-parser.add_argument("-f", "--file", help="filename inside ~/neo4j/import")
-parser.add_argument("-c", "--color", help="modifies node types for colored output in neo4j (does not work for metrics)", 
-                    action='store_true', default=False)
-args = parser.parse_args()
+import shutil
 
 config = configparser.ConfigParser()   
 config.read("config.txt")
@@ -19,24 +14,25 @@ config.read("config.txt")
 URI = config.get("neo4j", "url")
 AUTH = (config.get("neo4j", "user"), config.get("neo4j", "pass"))
 
-def upload_csv(file_url):
+def upload_csv(file_url:str, append_data: bool):
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
 
-        # Delete nodes
-        query = """
-                MATCH (n)-[r]-() DELETE r
-                """
+        if not append_data:
+                # Delete nodes
+                query = """
+                        MATCH (n)-[r]-() DELETE r
+                        """
+                
+                summary = driver.execute_query(query).summary
+                print(f"Deleted {summary.counters.relationships_deleted} edges")
         
-        summary = driver.execute_query(query).summary
-        print(f"Deleted {summary.counters.relationships_deleted} edges")
+                query = """
+                        MATCH (n) DELETE n;
+                        """
         
-        query = """
-                MATCH (n) DELETE n;
-                """
-        
-        summary = driver.execute_query(query).summary
-        print(f"Deleted {summary.counters.nodes_deleted} nodes")
+                summary = driver.execute_query(query).summary
+                print(f"Deleted {summary.counters.nodes_deleted} nodes")
         
         # Load nodes
         query = """
@@ -82,10 +78,54 @@ def upload_csv(file_url):
 
         print("Complete")
 
+def copy_file(src: str, dst: str):
+    """
+    Copy a file from src to dst.
+
+    :param src: Source file path
+    :param dst: Destination file path
+    """
+    try:
+        shutil.copy(src, dst)
+        print(f"File copied from {src} to {dst}")
+    except FileNotFoundError:
+        print(f"Source file {src} not found.")
+    except PermissionError:
+        print(f"Permission denied while copying to {dst}.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 if __name__ == "__main__":
-    #if args.file is not None and len(args.file) > 0  and os.path.isfile(args.file) :
-    if args.file is not None and len(args.file) > 0 :
-        file_url = "file:///" + args.file
+    parser = argparse.ArgumentParser("import_csv")
+    parser.add_argument(
+        "-f", "--file", required=True, help="filename inside ~/neo4j/import"
+    )
+    parser.add_argument(
+        "-C",
+        "--copy",
+        help="copy filename to ~/neo4j/import",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-a",
+        "--append",
+        help="do not delete the existing data",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-c",
+        "--color",
+        help="modifies node types for colored output in neo4j (does not work for metrics)",
+        action="store_true",
+        default=False,
+    )
+    args = parser.parse_args()
+    if args.file is not None and len(args.file) > 0  and os.path.isfile(args.file) :
+        filename = os.path.basename(args.file)
+        file_url = "file:///" + filename
         print(
                 "Uploading file", args.file
                 )
@@ -95,4 +135,6 @@ if __name__ == "__main__":
                 )
         parser.parse_args(['-h'])
         raise SystemExit()
-    upload_csv(file_url)
+    if args.copy:
+        copy_file(args.file, os.path.expanduser(f"~/neo4j/import/{filename}"))
+    upload_csv(file_url, args.append)
